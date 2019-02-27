2b15ca7be115a11e3cca441564a67c053f705adb
Ankur Rastogi <ankurras@usc.edu>
2019-02-26 17:16:41 -0800

    added routes

--- src/main/java/hello/Controller.java
+++ src/main/java/hello/Controller.java
@@ -13,32 +13,85 @@ public class Controller {
 	private final AtomicLong counter = new AtomicLong(0);
 	private ListManager listManager = new ListManager();
 
-	// @RequestMapping("/greeting")
-	// public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-	// 	return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	// }
+
+	///////////////////////////////////////////////////
+	// 												 //
+	// 			OUR ENDPOINTS AND ROUTES   		     //
+	// 												 //
+	///////////////////////////////////////////////////
 
 	@RequestMapping("/search")
 	public String handleSearchRequest(@RequestParam(defaultValue="null") String searchQuery, @RequestParam(defaultValue="5") Integer numResults) {
 
-		
-
 		return "Thanks for searching!";
-
 
 	}
 
+	// NOTE: this is a test endpoint that you can hit to make sure that you're actually adding a random item
+	// to the end of the favorites list.
 	@RequestMapping("/addItemToFavorites") 
 	public String addItemToFavorites() {
 		Result tempResult = new Result(String.valueOf(counter.incrementAndGet()), "{woo}");
 		listManager.addToList(tempResult, "favorites");
 		String favoritesString = listManager.getFavorites().toString();
-		return "favorites: " + favoritesString;
+		return "favorites: " + favoritesString + getTestString();
 	}
 
-	/*
+
+	// TODO: Need to write this.
+	@RequestMapping("/addToList")
+	public String handleAddToList(@RequestParam String itemToAdd, @RequestParam String targetListName) {
+		Result temp = new Result("1", "temp");
+		listManager.addToList(temp, targetListName);
+	}
+
+	// TODO: Need to write this.
+	@RequestMapping("/removeFromList")
+	public String handleRemoveFromList(@RequestParam String itemToRemoveId, @RequestParam String originListName) {
+		listManager.removeFromList(itemToRemoveId, originListName);
+	}
+
+	// TODO: Need to write this.
+	@RequestMapping("/moveBetweenLists")
+	public String handleMoveLists(@RequestParam String itemToRemoveId, @RequestParam String originListName, @RequestParam String originListName) {
+		listManager.removeFromList(itemToMoveId, originListName, targetListName);
+	}
 
 
-	The Greeting object must be converted to JSON. Thanks to Spring’s HTTP message converter support, you don’t need to do this conversion manually. Because Jackson 2 is on the classpath, Spring’s MappingJackson2HttpMessageConverter is automatically chosen to convert the Greeting instance to JSON.
-	*/
+
+
+
+
+	///////////////////////////////////////////////////
+	// 												 //
+	// 	EXTERNAL API INTERACTION AND PROCESSING      //
+	// NOTE: This is where the actual work happens.  //
+	// 												 //
+	///////////////////////////////////////////////////
+
+	
+	// TOOD: Need to write this. 
+	public ArrayList<Result> retrieveRestaurants(String searchQuery, Integer numResults) {
+		// TODO: Pull restaurants from external API and grab relevant information.
+		return new ArrayList<Result>();
+	}
+
+	// TOOD: Need to write this.
+	public ArrayList<Result> retrieveRecipes(String searchQuery, Integer numResults) {
+		// TODO: Pull recipes from external API and grab relevant information.
+		return new ArrayList<Result>();
+	}
+
+	// should take the searchQuery as a parameter and return a path to the collage. 
+	// NOTE: will likely need submethods here...
+	public String createCollage(String searchQuery) {
+		// TODO: Pull restaurants from external API and grab relevant information.
+
+		return "placeholder";
+	}
+
+
+	public String packageResponse(ArrayList<Result> restaurants, ArrayList<Result> recipes, String collagePath) {
+
+	}
 }
