02a4e8dbd6a1c163e3ad62a5f8d077fa0453a5eb
Ankur Rastogi <ankurras@usc.edu>
2019-02-28 16:59:27 -0800

    refactored result, recipe, restaurant classes

--- src/main/java/hello/Recipe.java
+++ src/main/java/hello/Recipe.java
@@ -1,9 +1,19 @@
 package hello;
 
+import java.util.ArrayList;
+
 
 // Result class which Recipe and Restaurant will both subclass off of...
 
 public class Recipe extends Result {
+
+	private String name;
+	private double numStars; // nnumber of stars out of 5
+	private double prepTime; // in mins
+	private double cookTime;
+	private ArrayList<String> ingredients; // by line, including quantity and item name
+	private ArrayList<String> instructions; // prep instructrions
+	private String sourceURL; // source url for our own reference...
 
 	public Recipe(String uniqueId, String jsonContent) {
 		super(uniqueId, jsonContent);
--- src/main/java/hello/Restaurant.java
+++ src/main/java/hello/Restaurant.java
@@ -5,10 +5,19 @@ package hello;
 
 public class Restaurant extends Result {
 
+	private String name;
+	private double rating;
+	private Integer drivingTime;
+	private String phoneNumber; 
+	private String address;
+	private String website;
+	private Integer priceLevel; // price range as expressed by Google Places API
+	private String placeId; // string pulled from Google Places API
+
+
 	public Restaurant(String uniqueId, String jsonContent) {
 		super(uniqueId, jsonContent);
-		// this.uniqueId = uniqueId;
-		// this.jsonContent = jsonContent;
+
 	}
 	
 }
