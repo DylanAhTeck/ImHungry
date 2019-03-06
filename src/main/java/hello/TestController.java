8cf67db04dab327cce3b03d9655c38e8b3c50625
Sally <tzuo@usc.edu>
2019-03-06 10:20:00 -0800

    set up junit test

---
+++ src/main/java/hello/TestController.java
@@ -0,0 +1,29 @@
+package hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRetrieveRestaurants() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRetrieveRecipes() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCreateCollage() {
+		fail("Not yet implemented");
+	}
+
+}
