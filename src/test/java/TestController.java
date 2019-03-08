e878fce93739e10aba7844aaf5ddb3bb5d22ddd9
Sally <tzuo@usc.edu>
2019-03-07 18:13:19 -0800

    uncomment retrive restaurant and change handle search request to retrieve restaurant in that method

--- src/test/java/TestController.java
+++ src/test/java/TestController.java
@@ -13,38 +13,36 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import hello.Controller;
-import hello.ListManager;
+import hello.Result;
 
 public class TestController {
 	
 	private static Controller controller;
-	private static ListManager listManager;
 
 	@BeforeClass
 	public static void setup() {
 		controller = new Controller();
-		listManager = new ListManager();
 	}
 	
 	//TODO: implement each method
 	
-//	@Test
-//	public void testRetrieveRestaurants() {
-//		String query = "burger"; // do not change the query
-//		int numResults = 20;
-//		
-//		Result fav = new Result("ChIJLyzMquXHwoAR0RpYK9bAM3M");
-//		ArrayList<Result> favorite = new ArrayList<Result>();
-//		favorite.add(fav);
-//		Result not = new Result("ChIJg6NxEefHwoARgGH0PL8Rx7A");
-//		ArrayList<Result> doNotShow = new ArrayList<Result>();
-//		doNotShow.add(not);
-//		controller.setDoNotShow(doNotShow);
-//		controller.setFav(favorite);
-//		controller.handleSearchRequest("null", numResults);
-//		controller.handleSearchRequest(query, 1);
-//		controller.handleSearchRequest(query, numResults);
-//	}
+	@Test
+	public void testRetrieveRestaurants() throws IOException {
+		String query = "burger"; // do not change the query
+		int numResults = 20;
+		
+		Result fav = new Result("ChIJLyzMquXHwoAR0RpYK9bAM3M");
+		ArrayList<Result> favorite = new ArrayList<Result>();
+		favorite.add(fav);
+		Result not = new Result("ChIJg6NxEefHwoARgGH0PL8Rx7A");
+		ArrayList<Result> doNotShow = new ArrayList<Result>();
+		doNotShow.add(not);
+		controller.setDoNotShow(doNotShow);
+		controller.setFav(favorite);
+		controller.retrieveRestaurants("null", numResults);
+		controller.retrieveRestaurants(query, 1);
+		controller.retrieveRestaurants(query, numResults);
+	}
 	
 //	@Test
 //	public void testRetrieveRecipes() {
